fee11f73ff8200daf6342a36a01571c78cdd7e61
Adam <adam.tecle@gmail.com>
2015-01-15 13:20:41 -0500

    attribution

--- scripts/csv2json.rb
+++ scripts/csv2json.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 require 'csv'
 require 'json'
- 
+
+# from here https://gist.github.com/enriclluelles/1423950
 if ARGV.size != 2
     puts 'Usage: csv_to_json input_file.csv output_file.json'
     puts 'This script uses the first line of the csv file as the keys for the JSON properties of the objects'
